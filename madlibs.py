ad8d1afde600d8aebca84baa5506e0264b62adf8
Emrullah DEMİR <e177demir@gmail.com>
2021-11-23 11:28:48 +0300

    Madlib exampele

---
+++ madlibs.py
@@ -0,0 +1,19 @@
+# # string concat
+# # let's say we want to create  "subscribe to _______"
+
+# youtuber = "Emrullah DEMİR" #some string variable
+
+# # ways to do this
+# print("Subscribe to " + youtuber)
+# print("Subscribe to {}".format(youtuber))
+# print(f"Subscribe to {youtuber}") # best practice
+
+adj = input("Adjective: ")
+verb1 = input("Verb: ")
+verb2 = input("Verb: ")
+famous_person = input("Famous person: ")
+
+madlib = f"Computer programming is so {adj}! It makes me so excited all the time because \
+I love to {verb1}. Stay hydrated and {verb2} like you are {famous_person}!"
+
+print(madlib)
